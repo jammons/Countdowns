33a1ae5d5b93c5c65e813ace1c24d225a6a533bd
Jeff Ammons <jeff@jeffammons.net>
2012-02-25 12:48:55 -0300

    Adding js files.

---
+++ countdown/counter/static/js/countdown.js
@@ -0,0 +1,17 @@
+// for my countdown js
+(function() {
+    Counter = Backbone.Model.extend({
+        // model for each counter
+        initialize: function(){
+        }
+    });
+
+    CounterView = Backbone.View.extend({
+        initialize: function(){
+
+        },
+        syncWithDOM: function(){
+
+        }
+    });
+})();
